bb115ecf2b7c2a7f21b5b33a10b6a7042cb95b8c
Elena Kopylova <108187256+kop-e-lova@users.noreply.github.com>
2023-07-21 10:26:11 +0300

    Create Settings.py

---
+++ Skill_search/Settings.py
@@ -0,0 +1,8 @@
+url_skill_search = "https://search.corp.cloveri.com/api/search"
+company_id = "4b871f91-45aa-4a72-8cb5-417ef561307f"
+profession_ids = ["9c08af9c-22d8-4bc6-a598-cb3ad035ceef"]
+team_ids = ["23a4363f-28d0-43e2-b66e-f2aedd4175ae"]
+skills =  [{"skill_id": "6796cfb9-a61c-48fd-ac9c-a395a001fad1"}, {"skill_id": "2c901b0c-f1cb-41e9-b566-a53f1b89714f"}, {"skill_id": "a3c1ef15-6d4f-4290-abee-62247c5465ff"}]
+skills_1 =  [{"skill_id": "6796cfb9-a61c-48fd-ac9c-a395a001fad1", "assessment_from": 0.1,"assessment_to": 0.9},
+             {"skill_id": "2c901b0c-f1cb-41e9-b566-a53f1b89714f", "assessment_from": 0.1, "assessment_to": 0.9},
+             {"skill_id": "a3c1ef15-6d4f-4290-abee-62247c5465ff", "assessment_from": 0.1,"assessment_to": 0.9}]
